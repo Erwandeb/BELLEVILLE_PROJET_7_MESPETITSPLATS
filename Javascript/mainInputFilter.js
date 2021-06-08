af221d4f67f1b452d27903368aa839123ab188df
Erwan de Belleville <edebelleville@hotmail.fr>
2021-06-08 10:44:35 +0200

    correction bug main bar

--- Javascript/mainInputFilter.js
+++ Javascript/mainInputFilter.js
@@ -17,15 +17,30 @@ mainSearchBarinput.addEventListener('keyup', (e) => {
     userSearchWord = e.target.value.toLowerCase();
 
     // Bloquage du input avant 3 lettres 
-    if(userSearchWord.length <= 2 ){
+    if(userSearchWord.length <= 2 && filterWordList.length === 0){
         noResultatBloc.innerHTML ="";
         e.preventDefault();
         e.stopPropagation();
         return;
-    } else{
+    } else if(userSearchWord.length >= 3 && filterWordList.length === 0){
         resultatFilter = allRecetteList.filter((recette) => {
-            let resultat = []
-        
+            let resultat = [];
+            for(const ingredient of recette.ingredients)
+                if(
+                    ingredient.ingredient.toLowerCase().includes(userSearchWord) ||
+                    recette.name.toLowerCase().includes(userSearchWord) ||
+                    recette.appliance.toString().toLowerCase().includes(userSearchWord) ||
+                    recette.ustensils.toString().toLowerCase().includes(userSearchWord) 
+                ){
+                    return resultat
+                }
+            
+            resultatFilter = resultat
+        })
+    } else if (userSearchWord.length <= 2 && filterWordList.length >= 1){
+        console.log("testing ok");
+        resultatFilter = resultatFilter.filter((recette) => {
+            let resultat = [];
             for(const ingredient of recette.ingredients)
                 if(
                     ingredient.ingredient.toLowerCase().includes(userSearchWord) ||
@@ -39,138 +54,18 @@ mainSearchBarinput.addEventListener('keyup', (e) => {
             resultatFilter = resultat
         })
     }
-    
-    console.log('filterWordList', filterWordListMainSearch);
-    console.log('resultat filter', resultatFilter);
+
 
     recetteDisplay(resultatFilter);
 
-    /*
-    for(element of allRecetteList){
-        if(userSearchWord.includes(element)){
-            console.log('hello de lu')
-            filterWordListMainSearch.push(userSearchWord);
-        }
-       
-    }
-    */
 
-  
-    
-    
-   
-    console.log('testing FWLISR', filterWordListMainBar);
-
-    // Algorithme de filtre
-   // filterMainSearchBarAlgorithme();
-
-})
-
-
-
-function filterMainSearchBarAlgorithme(){
-
-
-    /*
-    if(filterWordList.length >= 1 ){
-        filterWordList.filter((element) => {
-
-            resultatFilter = allRecetteList.filter((recette) => {
-                let resultat = []
-            
-                for(const ingredient of recette.ingredients)
-                    if(
-                        ingredient.ingredient.toLowerCase().includes(element) ||
-                        recette.name.toLowerCase().includes(element) ||
-                        recette.appliance.toString().toLowerCase().includes(element) ||
-                        recette.ustensils.toString().toLowerCase().includes(element) 
-                    ){
-                        return resultat
-                    }
-                
-                resultatFilter = resultat
-            })
-        })
-    } 
-*/
-
-if(filterWordList.length === 0){
-
-    filterWordListMainSearch.filter((element) => {
-        console.log('filterWordListMain', filterWordListMainBar);
-        console.log('bingoo');
-        resultatFilter = allRecetteList.filter((recette) => {
-            let resultat = []
-        
-            for(const ingredient of recette.ingredients)
-                if(
-                    ingredient.ingredient.toLowerCase().includes(element) ||
-                    recette.name.toLowerCase().includes(element) ||
-                    recette.appliance.toString().toLowerCase().includes(element) ||
-                    recette.ustensils.toString().toLowerCase().includes(element) 
-                ){
-                    return resultat
-                }
-            
-            resultatFilter = resultat
-        })
-        
-    })
-}
-
-    /*
-    filterWordList.filter((element) => {
-
-        resultatFilter = allRecetteList.filter((recette) => {
-            let resultat = []
-        
-            for(const ingredient of recette.ingredients)
-                if(
-                    ingredient.ingredient.toLowerCase().includes(element) ||
-                    recette.name.toLowerCase().includes(element) ||
-                    recette.appliance.toString().toLowerCase().includes(element) ||
-                    recette.ustensils.toString().toLowerCase().includes(element) 
-                ){
-                    return resultat
-                }
-            
-            resultatFilter = resultat
-        })
-        
-    })
-    */
-
-/*
-
-if(filterWordList.length >= 2 ){
-    filterWordList.filter((element) => {
-
-        resultatFilter = resultatFilter.filter((recette) => {
-            let resultat = []
-        
-            for(const ingredient of recette.ingredients)
-                if(
-                    ingredient.ingredient.toLowerCase().includes(element) ||
-                    recette.name.toLowerCase().includes(element) ||
-                    recette.appliance.toString().toLowerCase().includes(element) ||
-                    recette.ustensils.toString().toLowerCase().includes(element) 
-                ){
-                    return resultat
-                }
-            
-            resultatFilter = resultat
-        })
-        
-    })
-}
-*/
-
-
-    // Affichage des résultats sur ecran
-    recetteDisplay(resultatFilter);
-
-
-    if(filterWordList.length === 0 ){
+    if(filterWordList.length === 0 && userSearchWord.length === 0 ){
         recetteDisplay(allRecetteList);
     }
-}
+
+});
+
+
+
+
+   
